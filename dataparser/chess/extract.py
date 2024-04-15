cda14d2369826d88ca9e2f9e1d6bc1891593d809
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2024-04-15 16:45:41 -0400

    finished parser

--- dataparser/chess/extract.py
+++ dataparser/chess/extract.py
@@ -29,22 +29,45 @@ else:
 pgn_files = [file for file in os.listdir(extract_dir) if file.endswith(".pgn")]
 total_files = len(pgn_files)
 
+def get_winner(game):
+    result = game.headers["Result"]
+    
+    if result == "1-0":
+        return "white"
+    elif result == "0-1":
+        return "black"
+    else:
+        return None
+
 def process_file(game, json_file):
-    moves = []
-    board = chess.Board()
-    for num, move in enumerate(game.mainline_moves(), start=1):
-        moves.append(f"{num}. {board.san(move)}")
-        board.push(move)
-    json_file.write(json.dumps({"moves" : [{ "agent" : move } for move in moves]}, ensure_ascii=False))
+    winner = get_winner(game)
+    def role_of_index(i):
+        if winner == "white":
+            if i % 2 == 1: return "assistant"
+            else: return "agent"
+        elif winner == "black":
+            if i % 2 == 0: return "agent"
+            else: return "assistant"
+    if winner != None:
+        moves = []
+        board = chess.Board()
+        for num, move in enumerate(game.mainline_moves(), start=1):
+            moves.append(f"{num}.{board.san(move)}")
+            board.push(move)
+        json_file.write(json.dumps({"messages" : [{"role" : "system", "content" : f"{winner} won the following chess game"}] + \
+                                    [{ "role" : role_of_index(i), "content" : move} for i, move in enumerate(moves)]}, ensure_ascii=False))
+        json_file.write('\n')
 
     
+num_games = 0
 with io.open('chessdata.json', 'w', encoding='utf-8') as json_file:
     with tqdm(total=total_files, unit="file") as pbar:
         for file_name in pgn_files:
             pgn_file = os.path.join(extract_dir, file_name)
             with open(pgn_file, encoding='latin-1') as pgn:
                 while game := chess.pgn.read_game(pgn):
+                    num_games += 1
                     process_file(game, json_file)
             pbar.update(1)
 
-print(f"Extracted {len(games)} games from the dataset.")
+print(f"Extracted {num_games} games from the dataset.")
