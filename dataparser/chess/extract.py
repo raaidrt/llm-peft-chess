e1df68ff14d2a270f6d92259c7739b72fc3aeb84
Mihir Dhamankar <mihirmdkar@gmail.com>
2024-04-30 02:53:18 +0000

    generates 4437 lines of chess data.json

--- dataparser/chess/extract.py
+++ dataparser/chess/extract.py
@@ -6,6 +6,10 @@ import io
 import json
 from tqdm import tqdm
 from multiprocess import Pool
+import random 
+
+include_moves = False
+process_num_games = 10_000
 
 # Download the dataset
 url = "https://archive.org/download/KingBase2019/KingBase2019-pgn.zip"
@@ -29,6 +33,12 @@ else:
 # Extract the games from all the PGN files in the directory
 pgn_files = [file for file in os.listdir(extract_dir) if file.endswith(".pgn")]
 total_files = len(pgn_files)
+games_to_process_per_file = [ process_num_games // total_files ] * total_files
+if sum(games_to_process_per_file) < process_num_games:
+    games_to_process_per_file[-1] += process_num_games - sum(games_to_process_per_file)
+games_to_process_per_file_mapping = { }
+for i, file in enumerate(pgn_files):
+    games_to_process_per_file_mapping[file] = games_to_process_per_file[i]
 
 def get_winner(game):
     result = game.headers["Result"]
@@ -42,11 +52,20 @@ def get_winner(game):
 
 def process_file(file_name):
     pgn_file = os.path.join(extract_dir, file_name)
+    num_games_to_process = games_to_process_per_file_mapping[file_name]
+    lines = set(random.choices(list(range(num_games_to_process)), k=num_games_to_process))
+    # print(lines)
     with open(pgn_file, encoding='latin-1') as pgn:
         if not os.path.exists('data'):
             os.makedirs('data')
         with io.open(f'data/{file_name}.json', 'w', encoding='utf-8') as json_file:
+            line_i = 0
             while game := chess.pgn.read_game(pgn):
+                if line_i not in lines: 
+                    line_i += 1
+                    # print(f"index {line_i} is not in lines, moving on")
+                    continue
+                line_i += 1
                 winner = get_winner(game)
                 def role_of_index(i):
                     if winner == "white":
@@ -57,13 +76,28 @@ def process_file(file_name):
                         else: return "assistant"
                 if winner != None:
                     try:
+                        board_states = []
+                        legal_moves = []
                         moves = []
                         board = chess.Board()
-                        for num, move in enumerate(game.mainline_moves(), start=1):
-                            moves.append(f"{num}.{board.san(move)}")
+                        for num, move in enumerate(game.mainline_moves()):
+                            moves.append(f"{num // 2 + 1}{'.' * (1 if num % 2 == 0 else 3)}{board.san(move)}")
                             board.push(move)
+                            board_states.append(board.fen())
+                            legal_moves.append([board.san(move) for move in board.legal_moves])
                         json_file.write(json.dumps({"messages" : [{"role" : "system", "content" : f"{winner} won the following chess game"}] + \
-                                                    [{ "role" : role_of_index(i), "content" : move} for i, move in enumerate(moves)]}, ensure_ascii=False))
+                                                    [
+                                                    { 
+                                                        "role" : role_of_index(i), 
+                                                        "content" : move,
+                                                        "fen" : board_states[i],
+                                                        "legal_moves": legal_moves[i]
+                                                    } if include_moves else { 
+                                                        "role" : role_of_index(i), 
+                                                        "content" : move,
+                                                        "fen" : board_states[i],
+                                                    }
+                                                      for i, move in enumerate(moves)]}, ensure_ascii=False))
                         json_file.write('\n')
                     except Exception as e:
                         print(f"Error processing game in {pgn_file}: {e}")
